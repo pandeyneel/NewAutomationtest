0fcd2f78a22b4096f56c758ab8f1dd0b9e9e4ba5
Nilesh <pakash078@gmail.com>
2023-09-15 20:17:30 +0530

    Third commit

--- src/main/java/New_Automation/Batch/NewTest.java
+++ src/main/java/New_Automation/Batch/NewTest.java
@@ -1,5 +1,18 @@
 package New_Automation.Batch;
 
-public class NewTest {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import propertyutility.Baselibrary;
+
+public class NewTest extends Baselibrary
+{
+	public NewTest()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(xpath = "sfds")
+	private WebElement  nielsh;
 
 }
--- src/main/java/propertyutility/Baselibrary.java
+++ src/main/java/propertyutility/Baselibrary.java
@@ -1,5 +1,17 @@
 package propertyutility;
 
-public class Baselibrary {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Baselibrary 
+{public static WebDriver driver;
+	public  static void  getlaunch(String  url)
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get(url);
+	}
 
 }
