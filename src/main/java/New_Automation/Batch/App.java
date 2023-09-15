220b86f1234916efca8cd30b3def296f3096ec74
Nilesh <pakash078@gmail.com>
2023-09-15 19:36:41 +0530

    First Commit

---
+++ src/main/java/New_Automation/Batch/App.java
@@ -0,0 +1,13 @@
+package New_Automation.Batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/New_Automation/Batch/NewTest.java
@@ -0,0 +1,5 @@
+package New_Automation.Batch;
+
+public class NewTest {
+
+}
